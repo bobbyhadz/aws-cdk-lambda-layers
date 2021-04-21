b996bc7fd7d55ce6c5a7e79a3b67f21d044a98a2
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-21 20:55:34 +0300

    minor changes

--- src/my-lambda/index.ts
+++ src/my-lambda/index.ts
@@ -4,6 +4,7 @@ import {double} from '/opt/nodejs/calc';
 /* eslint-disable import/extensions, import/no-absolute-path */
 import {number, object, string} from '/opt/nodejs/yup-utils';
 
+// 👇 using yup layer
 const schema = object().shape({
   name: string().required(),
   age: number().required(),
@@ -17,6 +18,7 @@ export async function main(
   await schema.isValid({name: 'Tom', age: 24});
 
   return {
+    // 👇 using calc layer
     body: JSON.stringify({num: double(15)}),
     statusCode: 200,
   };
