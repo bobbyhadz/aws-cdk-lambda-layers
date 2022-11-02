93d77fe5a110c5b26f3d992ec84a97790fec8270
Borislav Hadzhiev <opmftw@gmail.com>
2022-11-02 20:59:23 +0200

    update CDK version

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -10,8 +10,8 @@ export class CdkStarterStack extends cdk.Stack {
     // 👇 layer we've written
     const calcLayer = new lambda.LayerVersion(this, 'calc-layer', {
       compatibleRuntimes: [
-        lambda.Runtime.NODEJS_12_X,
         lambda.Runtime.NODEJS_14_X,
+        lambda.Runtime.NODEJS_16_X,
       ],
       code: lambda.Code.fromAsset('src/layers/calc'),
       description: 'multiplies a number by 2',
@@ -20,8 +20,8 @@ export class CdkStarterStack extends cdk.Stack {
     // 👇 3rd party library layer
     const yupLayer = new lambda.LayerVersion(this, 'yup-layer', {
       compatibleRuntimes: [
-        lambda.Runtime.NODEJS_12_X,
         lambda.Runtime.NODEJS_14_X,
+        lambda.Runtime.NODEJS_16_X,
       ],
       code: lambda.Code.fromAsset('src/layers/yup-utils'),
       description: 'Uses a 3rd party library called yup',
@@ -31,7 +31,7 @@ export class CdkStarterStack extends cdk.Stack {
     new NodejsFunction(this, 'my-function', {
       memorySize: 1024,
       timeout: cdk.Duration.seconds(5),
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_16_X,
       handler: 'main',
       entry: path.join(__dirname, `/../src/my-lambda/index.ts`),
       bundling: {
