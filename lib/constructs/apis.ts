8aa29a4248a18a3f6b7740579c0f17f42de10b1d
hhimanshu <dev.atwork@yahoo.com>
2023-01-20 16:13:08 -0800

    feat(): remove commented out code

--- lib/constructs/apis.ts
+++ lib/constructs/apis.ts
@@ -42,41 +42,10 @@ export class Apis extends Construct {
             }]
         })
 
-        // addCorsOptions(users)
-
         new CfnOutput(this, "Api", {
             value: api.url
         } )
 
 
     }
-
-}
-
-export function addCorsOptions(apiResource: IResource) {
-    apiResource.addMethod('OPTIONS', new MockIntegration({
-        integrationResponses: [{
-            statusCode: '200',
-            responseParameters: {
-                'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
-                'method.response.header.Access-Control-Allow-Origin': "'*'",
-                'method.response.header.Access-Control-Allow-Credentials': "'false'",
-                'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
-            },
-        }],
-        passthroughBehavior: PassthroughBehavior.NEVER,
-        requestTemplates: {
-            "application/json": "{\"statusCode\": 200}"
-        },
-    }), {
-        methodResponses: [{
-            statusCode: '200',
-            responseParameters: {
-                'method.response.header.Access-Control-Allow-Headers': true,
-                'method.response.header.Access-Control-Allow-Methods': true,
-                'method.response.header.Access-Control-Allow-Credentials': true,
-                'method.response.header.Access-Control-Allow-Origin': true,
-            },
-        }]
-    })
 }
